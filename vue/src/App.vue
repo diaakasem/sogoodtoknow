8e732906db0348d0ca016e1cec7ee1f7704cb989
Diaa Kasem <me@diaa.me>
2020-10-25 10:47:56 +0200

    Add vue router

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,43 +1,10 @@
 <template>
-  <img alt="Vue logo"
-       src="./assets/logo-icon.png"
-       width="200"
-       height="200">
-  <Projects :projects="projects"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import axios from 'axios'
-import Projects from './components/Projects.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Projects
-  },
-  data: () => {
-    return {
-      projects: [{
-        title: 'Hello',
-        _id: '123'
-      }, {
-        title: 'World',
-        _id: '124'
-      }]
-    };
-  },
-  mounted() {
-    axios.post('http://localhost:4000/project/', {
-      status: 'created'
-    }).then((result) => {
-      console.log(result.data);
-      return this.projects = result.data;
-    }).catch(function (e){
-        console.error(e);
-    });
-  }
-}
-</script>
 
 <style>
 #app {
@@ -46,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="logo" src="../assets/logo-icon.png">
+    <Projects :projects="projects"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Projects from '@/components/Projects.vue'
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  components: {
+    Projects
+  },
+  mounted() {
+      axios.post('http://localhost:4000/project/', {
+        status: 'created'
+      }).then((result) => {
+        console.log(result.data);
+        return this.projects = result.data;
+      }).catch(function (e){
+          console.error(e);
+      });
+  }
+
+
+}
+</script>
